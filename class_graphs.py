1f902b8759bd2007d118a9dde6739274ea45ef5d
john culnan <jmculnan@email.arizona.edu>
2018-11-09 12:15:44 -0700

    changed data structure

--- class_graphs.py
+++ class_graphs.py
@@ -12,5 +12,22 @@ class CreateGraph(object):
     graph corresponding to the sentences in the input.
     """
     def __init__(self,data):
-        self.data   = addinput(data)
-        self.sents  = sent_tokenize(sents)
+        self.wds     = [word_tokenize(sent.lower()) for sent in data]
+        self.lems    = [wnl().lemmatize(wd) for sent in self.wds for wd in sent]
+        self.pos     = [item[1] for sent in self.wds for item in pos_tag(sent)]
+#        self.wd_pos  = ['%s_%s' % (wd, p) for wd in self.wds for p in self.pos]
+#        self.lem_pos = ['%s_%s' % (lem, p) for lem in self.lems for p in self.pos]
+
+    def connect_type(self,opt):
+        ctype = 'wd'
+        if opt == 'words':
+            ctype = 'wd'
+        elif opt == 'lemmas':
+            ctype = 'lem'
+        elif opt == 'word_pos':
+            ctype = 'wp'
+        elif opt == 'lemma_pos':
+            ctype = 'lp'
+
+
+coling = addinput('coling2016_explanation_sentences.txt')
--- helpers.py
+++ helpers.py
@@ -14,14 +14,18 @@ def addinput(data):
             all_data.append(line.strip())
     return all_data
 
+"""
 def preprocess(data):
-    """
+
     Takes a list of sentences and tokenizes,
     lemmatizes, and adds POS-tags.
-    """
+
     processed = []
     for sent in data:
+        lems = [wnl().lemmatize(wd) for wd in word_tokenize(sent)]
         sent = pos_tag(word_tokenize(sent))
-        [processed.append((lm,pos)) for (wd,pos) in sent for lm in wnl().lemmatize(wd,pos)]
-    #    processed.append(sent)
+        pos  = [item[1] for item in sent]
+        pairs= list(zip(lems,pos))
+        processed.append(pairs)
     return processed
+"""
