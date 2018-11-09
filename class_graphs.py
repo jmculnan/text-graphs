f1c03478eabafe2233d893a9f282830d94c54583
john culnan <jmculnan@email.arizona.edu>
2018-11-09 12:31:48 -0700

    restructuring data

--- class_graphs.py
+++ class_graphs.py
@@ -10,13 +10,16 @@ class CreateGraph(object):
     Instances of class CreateGraph are objects that
     take a corpus as input and create a computational
     graph corresponding to the sentences in the input.
+    The type of search used must be specified at the
+    time of input as 'words' 'lemmas', 'tagged words',
+    or 'tagged lemmas'. Default setting is 'words'.
     """
     def __init__(self,data):
-        self.wds     = [word_tokenize(sent.lower()) for sent in data]
-        self.lems    = [wnl().lemmatize(wd) for sent in self.wds for wd in sent]
-        self.pos     = [item[1] for sent in self.wds for item in pos_tag(sent)]
-#        self.wd_pos  = ['%s_%s' % (wd, p) for wd in self.wds for p in self.pos]
-#        self.lem_pos = ['%s_%s' % (lem, p) for lem in self.lems for p in self.pos]
+        self.data        = data
+        self.wds         = [word_tokenize(sent.lower()) for sent in data]
+        self.lems        = [wnl().lemmatize(wd) for sent in self.wds for wd in sent]
+        self.pos         = [item[1] for sent in self.wds for item in pos_tag(sent)]
+        self.connections = np.zeros((len(self.data),len(self.data)))
 
     def connect_type(self,opt):
         ctype = 'wd'
@@ -29,5 +32,7 @@ class CreateGraph(object):
         elif opt == 'lemma_pos':
             ctype = 'lp'
 
+    def getNodeSentence(NodeIdx):
+        return
 
 coling = addinput('coling2016_explanation_sentences.txt')
